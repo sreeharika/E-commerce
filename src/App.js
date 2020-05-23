43b1fa90ea3f4dbb648a01016a772bf4116a972a
sreeharika <sreeharikayelle@gmail.com>
2020-05-23 15:13:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+//nimport logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App(){
+  return(
+  <div className='App'>
+    <Navbar></Navbar>
+  {/* <Products></Products> */}
+    <Login></Login>
+    
+  </div>
+  )
 }
+
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class Navbar extends Component {
+    render() {
+      return(
+        <div>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Online Store</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="./Home.js">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./Login.js">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./Cart.js">Cart</a>
+      </li>
+      {/* <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li> */}
+    </ul>
+
+    
+  </div>
+</nav>
+        </div>
+      )
+
+        // return (
+        //     <div>
+        //         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        //           <a className="navbar-brand" href="#">Navbar</a>
+        //           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        //             <span className="navbar-toggler-icon"></span>
+        //           </button>
+        //           <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        //             <ul className="navbar-nav mr-auto">
+        //               <li className="nav-item active">
+        //                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        //               </li>
+        //               <li className="nav-item">
+        //                 <a className="nav-link" href="#">Login</a>
+        //               </li>
+        //               <li className="nav-item dropdown">
+        //                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        //                   Products
+        //                 </a>
+        //                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+        //                   <a className="dropdown-item" href="#">Women's</a>
+        //                   <a className="dropdown-item" href="#">Men's</a>
+        //                   <div className="dropdown-divider"></div>
+        //                   <a className="dropdown-item" href="#">Something else here</a>
+        //                 </div>
+        //               </li>
+        //               <li className="nav-item">
+        //                 <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        //               </li>
+        //             </ul>
+        //             <form className="form-inline my-2 my-lg-0">
+        //               {/* <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> */}
+        //               <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        //             </form>
+        //           </div>
+        //         </nav>
+        //     </div>
+        // )
+    }
+}
+export default Navbar;
